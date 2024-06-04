9129b0a9ece9e55d0a5f35ff0d37e6bf9cbce32d
Karola Kukowska <64924406+karola-kukowska@users.noreply.github.com>
2024-06-04 12:12:43 +0200

    notes update

--- 06-document-object-model/11-custom-insertafter-challenge/shopping-list/script.js
+++ 06-document-object-model/11-custom-insertafter-challenge/shopping-list/script.js
@@ -1,13 +1,13 @@
 function insertAfter(newEl, existingEl) {
-  existingEl.parentElement.insertBefore(newEl, existingEl.nextSibling);
+	existingEl.insertAdjacentElement("afterend", newEl);
 }
 
 // New element to insert
-const li = document.createElement('li');
-li.textContent = 'Insert Me After!';
+const li = document.createElement("li");
+li.textContent = "Insert Me After!";
 
 // Existing element to insert after
-const firstItem = document.querySelector('li:first-child');
+const firstItem = document.querySelector("li:first-child");
 
 // Our custom function
 insertAfter(li, firstItem);
