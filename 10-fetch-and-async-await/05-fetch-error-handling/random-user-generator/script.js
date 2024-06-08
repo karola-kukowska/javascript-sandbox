d619d595a1fe29c7ef7a637b3db45dca040885c5
Karola Kukowska <64924406+karola-kukowska@users.noreply.github.com>
2024-06-08 14:41:09 +0200

    random user miniproject

--- 10-fetch-and-async-await/05-fetch-error-handling/random-user-generator/script.js
+++ 10-fetch-and-async-await/05-fetch-error-handling/random-user-generator/script.js
@@ -1,44 +1,60 @@
-function fetchUser() {
-  showSpinner();
-  fetch('https://randomuser.me/api')
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error('Request Failed');
-      }
-
-      return res.json();
-    })
-    .then((data) => {
-      hideSpinner();
-      displayUser(data.results[0]);
-    })
-    .catch((error) => {
-      hideSpinner();
-      document.querySelector('#user').innerHTML = `
+async function getUser() {
+	try {
+		showSpinner();
+		const res = await fetch("https://randomuser.me/api/");
+		if (!res.ok) {
+			throw new Error();
+		}
+		const data = await res.json();
+		const {
+			email,
+			gender,
+			phone,
+			name: { first: firstName },
+			name: { last: lastName },
+			location: { city },
+			location: { country },
+			dob: { age },
+			picture: { large: img },
+		} = await data.results[0];
+		const user = {
+			email,
+			gender,
+			phone,
+			name: `${firstName} ${lastName}`,
+			location: `${city} ${country}`,
+			age,
+			img,
+		};
+		displayUser(user);
+		hideSpinner();
+	} catch (error) {
+		hideSpinner();
+		document.querySelector("#user").innerHTML = `
       <p class="text-xl text-center text-red-500 mb-5">
-      ${error}</p>`;
-    });
+      Oops... something went wrong :(</p>`;
+	}
 }
 
 function displayUser(user) {
-  const userDisplay = document.querySelector('#user');
+	const userDisplay = document.querySelector("#user");
 
-  if (user.gender === 'female') {
-    document.body.style.backgroundColor = 'rebeccapurple';
-  } else {
-    document.body.style.backgroundColor = 'steelblue';
-  }
+	if (user.gender === "female") {
+		document.body.style.backgroundColor = "rebeccapurple";
+	} else {
+		document.body.style.backgroundColor = "steelblue";
+	}
 
-  userDisplay.innerHTML = `
+	userDisplay.innerHTML = `
   <div class="flex justify-between">
   <div class="flex">
     <img
       class="w-48 h-48 rounded-full mr-8"
-      src="${user.picture.large}"
+      src="${user.img}"
     />
     <div class="space-y-3">
       <p class="text-xl">
-        <span class="font-bold">Name: </span>${user.name.first} ${user.name.last}
+        <span class="font-bold">Name: </span>${user.name}
       </p>
       <p class="text-xl">
         <span class="font-bold">Email: </span> ${user.email}
@@ -47,9 +63,9 @@ function displayUser(user) {
         <span class="font-bold">Phone: </span> ${user.phone}
       </p>
       <p class="text-xl">
-        <span class="font-bold">Location: </span> ${user.location.city} ${user.location.country}
+        <span class="font-bold">Location: </span> ${user.location}
       </p>
-      <p class="text-xl"><span class="font-bold">Age: </span> ${user.dob.age}</p>
+      <p class="text-xl"><span class="font-bold">Age: </span> ${user.age}</p>
     </div>
   </div>
 </div>
@@ -57,13 +73,13 @@ function displayUser(user) {
 }
 
 function showSpinner() {
-  document.querySelector('.spinner').style.display = 'block';
+	document.querySelector(".spinner").style.display = "block";
 }
 
 function hideSpinner() {
-  document.querySelector('.spinner').style.display = 'none';
+	document.querySelector(".spinner").style.display = "none";
 }
 
-document.querySelector('#generate').addEventListener('click', fetchUser);
+document.querySelector("#generate").addEventListener("click", getUser);
 
-fetchUser();
+getUser();
