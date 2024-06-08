91ed3602876a5f29aaf2500f5a957a66c586a1ad
Karola Kukowska <64924406+karola-kukowska@users.noreply.github.com>
2024-06-08 20:31:32 +0200

    todo miniproject

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,81 +1,136 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+const url = "https://jsonplaceholder.typicode.com/todos";
 
-const getTodos = () => {
-  fetch(apiUrl + '?_limit=10')
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((todo) => addTodoToDOM(todo));
-    });
-};
+async function getTodos() {
+	try {
+		//fetch todos
+		const res = await fetch(url + "?_limit=10");
+		if (!res.ok) {
+			thr;
+		}
+		const data = await res.json();
+		//display on screen
+		data.forEach((element) => {
+			displayTodo(element);
+		});
+	} catch (error) {
+		//display error msg on screen
+		document
+			.getElementById("todo-list")
+			.appendChild(document.createTextNode("Ooops... sth went wrong"));
+	}
+}
 
-const addTodoToDOM = (todo) => {
-  const div = document.createElement('div');
-  div.classList.add('todo');
-  div.appendChild(document.createTextNode(todo.title));
-  div.setAttribute('data-id', todo.id);
+function displayTodo(todo) {
+	//create div element for each item
+	const div = document.createElement("div");
+	div.classList.add("todo");
+	div.classList.toggle("done", todo.completed);
+	div.appendChild(document.createTextNode(todo.title));
+	div.setAttribute("data-id", todo.id);
+	const icon = document.createElement("i");
+	icon.classList = "fa-solid fa-xmark";
+	div.appendChild(icon);
+	document.getElementById("todo-list").appendChild(div);
+}
 
-  if (todo.completed) {
-    div.classList.add('done');
-  }
+async function addTodo(e) {
+	e.preventDefault();
+	try {
+		//get data from input
+		//alternative: e.target.firstElementChild.value
+		const title = document.getElementById("title").value;
 
-  document.getElementById('todo-list').appendChild(div);
-};
+		if (!title) {
+			alert("Please add a item");
+			return;
+		}
+		const newTodo = {
+			title,
+			completed: false,
+			userId: 1,
+		};
+		//PUT request on server
+		const res = await fetch(url, {
+			method: "POST",
+			body: JSON.stringify(newTodo),
+			headers: {
+				"Content-type": "application/json",
+			},
+		});
+		console.log(res);
 
-const createTodo = (e) => {
-  e.preventDefault();
+		//get id given back by server
+		const { id } = await res.json();
+		newTodo.id = id;
 
-  const newTodo = {
-    title: e.target.firstElementChild.value,
-    completed: false,
-  };
+		//display on screen
+		displayTodo(newTodo);
 
-  fetch(apiUrl, {
-    method: 'POST',
-    body: JSON.stringify(newTodo),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => addTodoToDOM(data));
-};
+		//reset form
+		document.getElementById("todo-form").reset();
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-const toggleCompleted = (e) => {
-  if (e.target.classList.contains('todo')) {
-    e.target.classList.toggle('done');
+async function removeTodo(e) {
+	// +++ Changed to X icon because I didn't like dbl click +++
+	try {
+		//DELETE request on server
+		const id = e.target.parentNode.attributes["data-id"].value;
+		const res = await fetch(`${url}/${id}`, {
+			method: "DELETE",
+		});
+		console.log(res);
 
-    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
-  }
-};
+		//remove from DOM
+		e.target.parentNode.remove();
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-const updateTodo = (id, completed) => {
-  fetch(`${apiUrl}/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({ completed }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
+async function toggleTodo(e) {
+	if (e.target.localName === "i") {
+		//if X icon clicked execute removeTodo instead
+		removeTodo(e);
+		return;
+	} else if (!e.target.classList.contains("todo")) {
+		//ignore event if any space outside div elements is clicked
+		return;
+	}
 
-const deleteTodo = (e) => {
-  if (e.target.classList.contains('todo')) {
-    const id = e.target.dataset.id;
-    fetch(`${apiUrl}/${id}`, {
-      method: 'DELETE',
-    })
-      .then((res) => res.json())
-      .then(() => e.target.remove());
-  }
-};
+	//toggle completed state on item
+	try {
+		const id = e.target.attributes["data-id"].value;
+		const isDone = e.target.classList.contains("done");
 
-const init = () => {
-  document.addEventListener('DOMContentLoaded', getTodos);
-  document.querySelector('#todo-form').addEventListener('submit', createTodo);
-  document
-    .querySelector('#todo-list')
-    .addEventListener('click', toggleCompleted);
-  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
-};
+		//change bg color on screen
+		e.target.classList.toggle("done");
+
+		//PATCH request on server
+		const res = await fetch(`${url}/${id}`, {
+			method: "PATCH",
+			body: JSON.stringify({
+				id: id,
+				completed: isDone,
+			}),
+		});
+
+		console.log(res);
+		if (!res.ok) {
+			throw new Error();
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function init() {
+	document.addEventListener("DOMContentLoaded", getTodos);
+	document.getElementById("todo-list").addEventListener("click", toggleTodo);
+	// document.getElementById("todo-list").addEventListener("dblclick", removeTodo);
+	document.getElementById("todo-form").addEventListener("submit", addTodo);
+}
 
 init();
