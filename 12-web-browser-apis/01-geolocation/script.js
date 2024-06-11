892b58293e40ba2809c59031712562a4c9bc9639
Karola Kukowska <64924406+karola-kukowska@users.noreply.github.com>
2024-06-11 22:36:36 +0200

    web api notes

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -1,57 +1,15 @@
-// getCurrentPosition()
+function curSucess(pos) {
+    const coords = pos.coords;
+    console.log("🚀 ~ curSucess ~ coords:", coords.latitude)
 
-function curSuccess(pos) {
-  const coords = pos.coords;
-
-  console.log(`Latitude: ${coords.latitude}`);
-  console.log(`Longitude: ${coords.longitude}`);
-  console.log(`Within: ${coords.accuracy} meters`);
-}
+};
 
 function curError(err) {
-  console.log(`Error: ${err.code} - ${err.message}`);
+    console.log("🚀 ~ curError ~ err:", err)
 }
 
-const curOptions = {
-  enableHighAccuracy: true, // Use GPS if available
-  timeout: 5000, // Time to wait to stop trying for location
-  maximumAge: 0, // Do not use a cached position
-};
+const options = {};
 
-// navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
+navigator.geolocation.getCurrentPosition(curSucess, curError, options);
 
-// watchPosition()
-
-const target = {
-  latitude: 41.3874387,
-  longitude: -71.394839,
-};
-
-function watchSuccess(pos) {
-  const coords = pos.coords;
-  console.log(coords);
-
-  if (
-    target.latitude === coords.latitude &&
-    target.longitude === coords.longitude
-  ) {
-    console.log('You have reached your destination!');
-    navigator.geolocation.clearWatch(id);
-  }
-}
-
-function watchError(err) {
-  console.log(`Error: ${err.code} - ${err.message}`);
-}
-
-const watchOptions = {
-  enableHighAccuracy: true, // Use GPS if available
-  timeout: 5000, // Time to wait to stop trying for location
-  maximumAge: 0, // Do not use a cached position
-};
-
-const id = navigator.geolocation.watchPosition(
-  watchSuccess,
-  watchError,
-  watchOptions
-);
+//more notes in solution.js
